2bf5802dc7669e868582d402ca5a46ddefc153eb
Bsc1967 <112431342+Bsc1967@users.noreply.github.com>
2026-08-17 21:38:04 +0200

    Fix EVBox cmd34 remote start config mapping

--- components/evbox_max/evbox_max.h
+++ components/evbox_max/evbox_max.h
@@ -151,6 +151,7 @@ class EvboxMaxComponent : public Component, public uart::UARTDevice {
   void send_periodic_cmd18_();
   void send_meter_update_interval_();
   void send_config_request_();
+  bool send_remote_start_config_enable_(const std::string &config);
   bool send_remote_start_();
   void send_remote_stop_();
   void log_autostart_config_(const std::string &config);
@@ -232,8 +233,9 @@ class EvboxMaxComponent : public Component, public uart::UARTDevice {
   uint32_t startup_step_due_ms_{0};
   uint8_t startup_step_{0};
   bool startup_config_received_{false};
+  bool remote_start_config_write_attempted_{false};
+  bool remote_start_config_verified_{false};
   uint32_t last_startup_sync_request_ms_{0};
-  std::string pending_config_34_ {};
   ESPPreferenceObject settings_pref_{};
   bool janitza_online_{false};
 
